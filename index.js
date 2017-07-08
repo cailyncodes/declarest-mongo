06c7fd6ab825c3b5a27603595e08bf629606155f
Cole Hansen <cole_hansen@brown.edu>
2017-07-08 11:42:10 -0400

    Update way declarest is called
    Declarest is now a class so multiple instances can be created

--- index.js
+++ index.js
@@ -3,30 +3,123 @@ const fs = require('fs');
 const MongoClient = require('mongodb');
 const Hapi = require('hapi');
 
-let main = (function(file, options) {
-  options = options || {};
-  let data = fs.readFileSync(file);
-  let structure = yaml.load(data);
-  validate(structure);
-  
-  const server = new Hapi.Server();
-  server.connection({ port: options.port || 3000, host: options.host || '0.0.0.0' });
+module.exports = class {
+  constructor(fileName) {
+    this.load(fileName);
+    return this;
+  }
 
+  load(fileName) {
+    let file = fs.readFileSync(fileName);
+    this.structure = yaml.load(file);
 
-  MongoClient.connect(structure.uri, function(err, db) {
-    console.log("Connected successfully to database");
+    validate(this.structure);
 
-    addRoutes(structure.routes, server, db);
+    let db = this.__getConnection();
+    db.then((db) => {
+      this.db = db;
+    })
+    .catch((err) => {
+      throw new Error("There was an error connecting to the database.");
+    });
+
+    this.routes = this.__constructRoutes();
+  }
+
+  start(options) {
+    options = options || {};
+
+    // create server
+    this.server = new Hapi.Server();
+    this.server.connection({ port: options.port || 3000, host: options.host || '0.0.0.0' });
+
+    this.__addRoutes();
 
     // start the rest api server
-    server.start((err) => {
+    this.server.start((err) => {
       if (err) {
         throw err;
       }
-      console.log(`Server running at: ${server.info.uri}`);
+      console.log(`Server running at: ${this.server.info.uri}`);
     });
-  });  
-});
+  }
+
+  __getConnection() {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(this.structure.uri, function(err, db) {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(db);
+        return;
+      });
+    });
+  }
+
+  // add all the routes to the server object
+  __addRoutes() {
+    this.routes.forEach((route) => {
+      this.server.route(route);
+    });
+  }
+  
+  __constructRoutes() {
+    let routes = this.structure.routes;
+    return routes.map((route) => {
+      return this.__constructRoute(route);
+    });
+  }
+
+  // construct a route based on the structure
+  __constructRoute(route) {
+    let path = Object.keys(route)[0];
+    route = route[path];
+    let method = route.method || 'GET';
+    let handler = this.__constructHandler(method, route);
+    return {
+      method: method,
+      path: path,
+      handler: handler
+    };
+  }
+
+  // construct the handler that will interact with
+  // mongodb pursuant to the structure
+  __constructHandler(method, route) {
+    let handler;
+    switch (method) {
+      case 'GET':
+        handler = this.__constructGetHandler(route);
+        break;
+      default:
+        console.error("Invalid method found.");
+        break;
+    }
+    return handler;
+  }
+
+  __constructGetHandler(route) {
+    let filters = route.filter || [];
+    return (function(req, resp) {
+      let collection = this.db.collection(route.collection);
+      let cursor = collection.find({});
+
+      // add filters to the search
+      for (let filter of filters) {
+        cursor = cursor.filter(filter);
+      }
+      
+      cursor.toArray(function(err, docs) {
+        if (err) {
+          console.error(err);
+          resp("Oh oh!");
+        }
+        resp(docs);
+      });
+    }).bind(this);
+  }
+}
 
 // validate the syntax of the structure file
 function validate(structure) {
@@ -40,61 +133,3 @@ function validate(structure) {
   if (failed) new Error("Check error logs for errors.");
   return;
 }
-
-// add all the routes to the server object
-function addRoutes(routes, server, db) {
-  routes.forEach((route) => {
-    server.route(constructRoute(route, db));
-  });
-}
-
-// construct a route based on the structure
-function constructRoute(route, db) {
-  let path = Object.keys(route)[0];
-  route = route[path];
-  let method = route.method || 'GET';
-  let handler = constructHandler(method, route, db);
-  return {
-    method: method,
-    path: path,
-    handler: handler
-  };
-}
-
-// construct the handler that will interact with
-// mongodb pursuant to the structure
-function constructHandler(method, route, db) {
-  let handler;
-  switch (method) {
-    case 'GET':
-      handler = constructGetHandler(route, db);
-      break;
-    default:
-      console.error("Invalid method found.");
-      break;
-  }
-  return handler;
-}
-
-function constructGetHandler(route, db) {
-  let filters = route.filter || [];
-  return function(req, resp) {
-    let collection = db.collection(route.collection);
-    let cursor = collection.find({});
-
-    // add filters to the search
-    for (let filter of filters) {
-      cursor = cursor.filter(filter);
-    }
-    
-    cursor.toArray(function(err, docs) {
-      if (err) {
-        console.error(err);
-        resp("Oh oh!");
-      }
-      resp(docs);
-    });
-  }
-}
-
-module.exports = main;
