f7b07936e928158c0432ca018ed4f7b0df07501f
Cole Hansen <cole_hansen@brown.edu>
2017-07-08 15:03:36 -0400

    Update examples

--- examples/test.js
+++ examples/test.js
@@ -4,7 +4,3 @@ const path = require('path');
 let file = path.resolve(__dirname, 'example1.yaml');
 
 new Declarest(file).start();
-
-file = path.resolve(__dirname, 'example2.yaml');
-
-new Declarest(file).start({port:3001});
